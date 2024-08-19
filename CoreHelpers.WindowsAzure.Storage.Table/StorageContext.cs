644e8d24708df3f0458f87a5587bec76db6fb7e8
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-08-19 22:24:03 +0200

    fix formatting

--- CoreHelpers.WindowsAzure.Storage.Table/StorageContext.cs
+++ CoreHelpers.WindowsAzure.Storage.Table/StorageContext.cs
@@ -5,18 +5,18 @@ using Azure.Data.Tables;
 namespace CoreHelpers.WindowsAzure.Storage.Table
 {
     public partial class StorageContext : IStorageContext
-	{						
-		private IStorageContextDelegate _delegate { get; set; }		
-		private TableServiceClient tableServiceClient { get; set; }
+    {
+        private IStorageContextDelegate _delegate { get; set; }
+        private TableServiceClient tableServiceClient { get; set; }
 
         public StorageContext(string storageAccountName, string storageAccountKey, string storageEndpointSuffix = null)
-		{
+        {
             if (!String.IsNullOrEmpty(storageEndpointSuffix))
                 tableServiceClient = new TableServiceClient(string.Format("DefaultEndpointsProtocol={0};AccountName={1};AccountKey={2};EndpointSuffix={3}", "https", storageAccountName, storageAccountKey, storageEndpointSuffix));
             else
                 tableServiceClient = new TableServiceClient(string.Format("DefaultEndpointsProtocol={0};AccountName={1};AccountKey={2}", "https", storageAccountName, storageAccountKey));
-        
-		}
+
+        }
 
         public StorageContext(string connectionString)
         {
@@ -29,41 +29,41 @@ namespace CoreHelpers.WindowsAzure.Storage.Table
         }
 
         public StorageContext(StorageContext parentContext)
-		{            
+        {
             // we reference the entity mapper
             _entityMapperRegistry = new Dictionary<Type, StorageEntityMapper>(parentContext._entityMapperRegistry);
 
             // we are using the delegate
-			this.SetDelegate(parentContext._delegate);
+            this.SetDelegate(parentContext._delegate);
 
-			// take the tablename prefix
-			_tableNamePrefix = parentContext._tableNamePrefix;
+            // take the tablename prefix
+            _tableNamePrefix = parentContext._tableNamePrefix;
 
             // store the connection string
             tableServiceClient = parentContext.tableServiceClient;
         }
-        
+
         public void Dispose()
-		{}
+        { }
 
-		public void SetDelegate(IStorageContextDelegate delegateModel)
-			=> _delegate = delegateModel;
+        public void SetDelegate(IStorageContextDelegate delegateModel)
+            => _delegate = delegateModel;
 
-		public IStorageContextDelegate GetDelegate()
-			=> _delegate;
+        public IStorageContextDelegate GetDelegate()
+            => _delegate;
 
-		public IStorageContext CreateChildContext()
-			=> new StorageContext(this);
-			        						
+        public IStorageContext CreateChildContext()
+            => new StorageContext(this);
+
         public TableClient GetTableClient<T>()
         {
-			var tableName = GetTableName<T>();
-			return GetTableClient(tableName);
+            var tableName = GetTableName<T>();
+            return GetTableClient(tableName);
         }
 
         private TableClient GetTableClient(string tableName)
         {
-			return tableServiceClient.GetTableClient(tableName);
-        }                                         
+            return tableServiceClient.GetTableClient(tableName);
+        }
     }
 }
